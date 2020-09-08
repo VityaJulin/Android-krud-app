33ca72dd06c1a40a9fd1534c7ebf8d47cdd59562
vityajulin <vkdvornokov@gmail.com>
2020-09-08 19:56:51 +0300

    change posts list to mutable

--- app/src/main/java/com/example/android_krud_app/FeedActivity.kt
+++ app/src/main/java/com/example/android_krud_app/FeedActivity.kt
@@ -39,7 +39,7 @@ class FeedActivity : AppCompatActivity(),
             if (result.isSuccessful) {
                 with(container) {
                     layoutManager = LinearLayoutManager(this@FeedActivity)
-                    adapter = PostAdapter(result.body() ?: emptyList()).apply {
+                    adapter = PostAdapter((result.body() ?: emptyList()) as MutableList<PostModel>).apply {
                         likeBtnClickListener = this@FeedActivity
                     }
                 }
